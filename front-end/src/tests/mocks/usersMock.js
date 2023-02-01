c36945be89972f48642287d0d57078f9e1741717
Jonathan <jonathankarlinski57@gmail.com>
2023-01-31 21:16:22 -0300

    test: Cria testes para pagina login

--- front-end/src/tests/mocks/usersMock.js
+++ front-end/src/tests/mocks/usersMock.js
@@ -24,3 +24,55 @@ export const outputCustomerMock = {
   token: 'customer token',
   role: 'customer',
 };
+
+export const inputSellerMock = {
+  email: 'fulana@deliveryapp.com',
+  password: 'fulana@123',
+};
+
+export const outputSellerMock = {
+  id: 3,
+  name: 'Fulana Pereira',
+  email: 'fulana@deliveryapp.com',
+  token: 'seller token',
+  role: 'seller',
+};
+
+export const outputAdminMock = {
+  id: 1,
+  name: 'Delivery App Admin',
+  email: 'adm@deliveryapp.com',
+  role: 'administrator',
+};
+export const inputAdminMock = {
+  email: 'adm@deliveryapp.com',
+  password: '--adm2@21!!--',
+};
+
+export const allUsers = [
+  {
+    email: 'zebiritajr@email.com',
+    id: 4,
+    name: 'Zé Birita Jr',
+    role: 'customer',
+  },
+  {
+    email: 'aluisio@gmail.com',
+    id: 5,
+    name: 'Aluisio Mercadante',
+    role: 'seller',
+  },
+];
+
+export const orders = [
+  {
+    deliveryAddress: 'Rua oliveira',
+    deliveryNumber: '456',
+    id: 1,
+    saleDate: '2023-01-31T22:16:46.000Z',
+    sellerId: 2,
+    status: 'Pendente',
+    totalPrice: '12.19',
+    userId: 3,
+  },
+];
